25783323cb75ea7b2e07e6cc5a2bb6533653e221
TAMteam <BooBooGorgy@gmail.com>
2013-04-20 13:16:22 +0800

    Mod's Common

---
+++ mgm_common/TAM/mods/mgm/MythicalGods.java
@@ -0,0 +1,41 @@
+package TAM.mods.mgm;
+
+import TAM.mods.mgm.lib.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+@NetworkMod(channels = {Reference.MOD_ID}, clientSideRequired = true, serverSideRequired = false)
+public class MythicalGods {
+    
+@PreInit
+public void preInit(FMLPreInitializationEvent event){
+    
+    
+    
+    
+   }
+
+@Init
+public void init(FMLInitializationEvent event){
+    
+    
+    
+    
+   }
+
+@PostInit
+public void postInit(FMLPostInitializationEvent event){
+    
+    
+    
+    
+    
+   }
+}
---
+++ mgm_common/TAM/mods/mgm/lib/Reference.java
@@ -0,0 +1,9 @@
+package TAM.mods.mgm.lib;
+
+
+
+public class Reference {
+   public static final String MOD_ID = "MGM";
+   public static final String MOD_NAME = "Mythical Gods Mod";
+   public static final String VERSION = "Alpha 1.0";
+}
