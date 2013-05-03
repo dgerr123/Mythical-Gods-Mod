4eb197b75b3758db16710e4f7f68035c0b857e94
Msvisser <info@webmichiel.nl>
2013-05-03 12:10:15 +0200

    Changed recipes

--- mgm_common/com/TamModding/mgm/lib/RecipeHelper.java
+++ mgm_common/com/TamModding/mgm/lib/RecipeHelper.java
@@ -1,20 +1,19 @@
 package com.TamModding.mgm.lib;
 
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class RecipeHelper {
 
-    
     /**
      * Initializes all the Recipes
      */
-    
     public static void init() {
         // Add crafting recipes
-        GameRegistry.addRecipe(new ItemStack(ItemHelper.holyGem), new Object[] { " G ", "GDG", " G ", 'D', Item.diamond, 'G', ItemHelper.unholyGem });
-        GameRegistry.addRecipe(new ItemStack(ItemHelper.unholySword), new Object[] { " G ", " G ", " S ", 'S', Item.stick, 'G', ItemHelper.unholyGem });
-        GameRegistry.addRecipe(new ItemStack(ItemHelper.holySword), new Object[] { " G ", " G ", " S ", 'S', Item.stick, 'G', ItemHelper.holyGem });
+        GameRegistry.addRecipe(new ItemStack(ItemHelper.holyGem), new Object[] { "DGD", "GBG", "DGD", 'D', Item.diamond, 'G', ItemHelper.unholyGem, 'B', Block.blockDiamond });
+        GameRegistry.addRecipe(new ItemStack(ItemHelper.unholySword), new Object[] { " G ", "NGN", " S ", 'S', Item.blazeRod, 'G', ItemHelper.unholyGem, 'N', Item.netherStar });
+        GameRegistry.addRecipe(new ItemStack(ItemHelper.holySword), new Object[] { " G ", " G ", " S ", 'S', ItemHelper.unholySword, 'G', ItemHelper.holyGem });
     }
 }
