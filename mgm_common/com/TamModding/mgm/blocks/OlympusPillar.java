26383a494e5b39bf17cfe431a71b1afc00d08b81
Msvisser <info@webmichiel.nl>
2013-05-06 13:28:42 +0200

    Fixed bottom texture of pillar

--- mgm_common/com/TamModding/mgm/blocks/OlympusPillar.java
+++ mgm_common/com/TamModding/mgm/blocks/OlympusPillar.java
@@ -6,7 +6,7 @@ public class OlympusPillar extends BlocksBase {
 
     public OlympusPillar(int par1) {
         super(par1, Material.rock);
-        super.registerTexture("OlympusPillarSide", "OlympusPillarTop", "OlympusPillarBottom");
+        super.registerTexture("OlympusPillarSide", "OlympusPillarTop", "OlympusPillarTop");
         this.setHardness(3.0F);
         this.setResistance(10.0F);
     }
