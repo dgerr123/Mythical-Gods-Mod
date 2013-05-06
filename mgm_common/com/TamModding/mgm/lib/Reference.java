42ea87f648a87ac12e5cca21188bd4791428bc95
Msvisser <info@webmichiel.nl>
2013-05-06 19:05:02 +0200

    Moved the cape files

--- mgm_common/com/TamModding/mgm/lib/Reference.java
+++ mgm_common/com/TamModding/mgm/lib/Reference.java
@@ -12,10 +12,10 @@ public class Reference {
     public static final String PATH_TEXTURES = PATH_RESOURCES + "textures/";
     
     /* Paths to the cape files */
-    public static final String PATH_CAPES_DEV = "https://dl.dropboxusercontent.com/u/31038502/TAM%20Capes/dev.png";
-    public static final String PATH_CAPES_TESTER = "https://dl.dropboxusercontent.com/u/31038502/TAM%20Capes/tester.png";
-    public static final String HTTP_CAPES_DEV = "http://tammodding.com/dev.txt";
-    public static final String HTTP_CAPES_TESTER = "http://tammodding.com/tester.txt";
+    public static final String PATH_CAPES_DEV = "http://tammodding.com/capes/dev.png";
+    public static final String PATH_CAPES_TESTER = "http://tammodding.com/capes/tester.png";
+    public static final String HTTP_CAPES_DEV = "http://tammodding.com/capes/dev.txt";
+    public static final String HTTP_CAPES_TESTER = "http://tammodding.com/capes/tester.txt";
     public static final int CAPE_TICK_SKIP = 200; // Time in ticks (20 tick = 1 sec)
     
 }
